d40481b2a8bba06eb5d0fff1942e40906691eeda
Vinoth Jayaram <c_vinoja@codeaurora.org>
2023-11-11 06:29:08 +0000

    mido: Updated Mapper & Allocator

--- device.mk
+++ device.mk
@@ -173,7 +173,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.msm8953 \
@@ -183,6 +184,7 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libvulkan \
     memtrack.msm8953 \
+    vendor.qti.hardware.display.allocator-service \
     vendor.display.config@1.0.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor
 
